70fc3202512e29c4c6f0c06e69b39be17870391b
geetagad <geeta.gad@gmail.com>
2013-07-10 01:11:29 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var data = "";
 
 var app = express.createServer(express.logger());
 
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( data);
 });
 
 var port = process.env.PORT || 5000;
